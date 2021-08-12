1e04693f7666449755683beba98e1768b3d5a1aa
Lucas <luccas.a.pe@gmail.com>
2021-08-12 14:31:19 -0300

    Se creo el modelo y tokenService

--- src/app/modelo/nuevo-usuario.ts
+++ src/app/modelo/nuevo-usuario.ts
@@ -1,2 +1,23 @@
 export class NuevoUsuario {
+
+    nombre: string;
+    apellido: string;
+    nombreUsuario: string;
+    email: string;
+    password: string;
+    authorities: string[]=[]; 
+    roles: string[]=[];
+    
+    
+    
+    constructor(nombre: string, apellido: string , nombreUsuario: string, email:string, password: string){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.nombreUsuario=nombreUsuario;
+        this.email=email;
+        this.password=password;
+        
+
+    }
+
 }
