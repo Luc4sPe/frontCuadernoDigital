cc1b54c93bb1edad05b2d1329728319678d21bd9
Lucas <luccas.a.pe@gmail.com>
2021-09-21 14:19:24 -0300

    Se modifico, nuevo usuario, login, actualizar y listar

--- src/app/auth/usuario/nuevo/nuevo-usuario.component.ts
+++ src/app/auth/usuario/nuevo/nuevo-usuario.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { NuevoUsuario } from 'src/app/modelo/nuevo-usuario';
 import { AuthService } from 'src/app/service/auth.service';
 import { TokenService } from 'src/app/service/token.service';
+
 
 
 @Component({
@@ -16,74 +17,155 @@ import { TokenService } from 'src/app/service/token.service';
 export class NuevoUsuarioComponent implements OnInit {
   
 
-  usuario= new NuevoUsuario('','','','','','');
-  formUsuario!: FormGroup;
-  rol!:string ;
-  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //usuario= new NuevoUsuario('','','','','','');
+  //formUsuario!: FormGroup;
+  //rol!:string ;
+  //private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   
+  nombre: string='';
+  apellido: string='';
+  dni: string='';
+  nombreUsuario: string='';
+  email: string='';
+  password: string='';
+  msj: string='';
+  nuevoUsuario: NuevoUsuario | any;
+  roles=[
+    {
+    name:'Admin',
+    active: false,
+    description:'',
+  },
+  {
+    name:'User',
+    active: false,
+    description:'Rol por defecto',
+  },
+
+  {
+    name:'Encargado Agricola',
+    active: false,
+    description:'',
+  },
+
+  {
+    name:'Productor',
+    active: false,
+    description:'',
+  },
+]
   
-  
-  constructor( private service: AuthService, private router: Router, private tokenService: TokenService) { }
+  constructor( private service: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    this.buildFrom();
+    //this.buildFrom();
   
     
   }
-  async Guardar(){   
-    
-    if(this.rol === "Admin"){
-      
-      this.usuario.roles.push('Admin');
-      
-    }
-    if(this.rol === "Encargado Agricola"){
-      
-      this.usuario.roles.push('Encargado Agricola');
-      
-    }
-    if(this.rol === "Productor"){
-      
-      this.usuario.roles.push('Productor');
-      
-    }
-      await console.log(this.usuario.roles);
-      await this.service.nuevo(this.usuario).subscribe(data=>{
 
+
+  registrarUsuario(form: NgForm):  void{
+    let rolUsuarios = this.crearRoles();
+    this.nuevoUsuario = new NuevoUsuario(
+      this.nombre, this.apellido,
+      this.dni, this.nombreUsuario,
+      this.email, this.password,
+      rolUsuarios
+    );
+
+    this.service.nuevo(this.nuevoUsuario).subscribe(
+      data =>{
+        this.msj = data.mensaje;
         Swal.fire({
-          position: 'top',
           icon: 'success',
-          title: 'usuario Creado',
-          showConfirmButton: false,
-          timer: 1500
-        })
-        this.router.navigate(["usuario"]);
+          title: this.msj,
+          text: '',
+        });
+        
+        this.resetiarRoles();
+        form.resetForm();
+      },
+      err =>{
+        this.msj = err.error.mensaje;
+        Swal.fire({
+          icon: 'error',
+          title: '',
+          text: this.msj,
+        });        
+      }
+    )
 
-       })
-    
-    
-  
   }
 
 
-    get nombre(){return this.formUsuario.get('nombre');}
-    get apellido(){return this.formUsuario.get('apellido');}
-    get dni(){return this.formUsuario.get('dni');}
-    get nombreUsuario(){return this.formUsuario.get('nombreUsuario');}
-    get email(){return this.formUsuario.get('email');}
-    get password(){return this.formUsuario.get('password');}
-    get roles(){return this.formUsuario.get('roles');}
+  crearRoles(): string[] {
+    let result = this.roles
+        .filter((role) => role.active === true)
+        .map((role) => role.name); 
+    return result;
+  }
 
-    private buildFrom(){
-      this.formUsuario = new FormGroup({
-        nombre: new FormControl('',[Validators.required]),
-        apellido: new FormControl('',[Validators.required]),
-        dni: new FormControl('',[Validators.required]),
-        nombreUsuario: new FormControl('',[Validators.required]),
-        email: new FormControl('',[Validators.required, Validators.pattern(this.emailPattern)]),
-        password: new FormControl('',[Validators.required]),
-        roles: new FormControl('',[Validators.required]),
-      });
+  resetiarRoles(): void {
+    this.roles
+      .filter((role) => role.name != 'User')
+      .forEach((role) => role.active = false);
+    
+  }
+
+  // async Guardar(){   
+    
+  //   if(this.rol === "Admin"){
+      
+  //     this.usuario.roles.push('Admin');
+      
+  //   }
+  //   if(this.rol === "Encargado Agricola"){
+      
+  //     this.usuario.roles.push('Encargado Agricola');
+      
+  //   }
+  //   if(this.rol === "Productor"){
+      
+  //     this.usuario.roles.push('Productor');
+      
+  //   }
+  //     await console.log(this.usuario.roles);
+  //     await this.service.nuevo(this.usuario).subscribe(data=>{
+
+  //       Swal.fire({
+  //         position: 'top',
+  //         icon: 'success',
+  //         title: 'usuario Creado',
+  //         showConfirmButton: false,
+  //         timer: 1500
+  //       })
+  //       this.router.navigate(["usuario"]);
+
+  //      })
+    
+    
+  
+  // }
+
+
+    // get nombre(){return this.formUsuario.get('nombre');}
+    // get apellido(){return this.formUsuario.get('apellido');}
+    // get dni(){return this.formUsuario.get('dni');}
+    // get nombreUsuario(){return this.formUsuario.get('nombreUsuario');}
+    // get email(){return this.formUsuario.get('email');}
+    // get password(){return this.formUsuario.get('password');}
+    // get roles(){return this.formUsuario.get('roles');}
+
+    // private buildFrom(){
+    //   this.formUsuario = new FormGroup({
+    //     nombre: new FormControl('',[Validators.required]),
+    //     apellido: new FormControl('',[Validators.required]),
+    //     dni: new FormControl('',[Validators.required]),
+    //     nombreUsuario: new FormControl('',[Validators.required]),
+    //     email: new FormControl('',[Validators.required, Validators.pattern(this.emailPattern)]),
+    //     password: new FormControl('',[Validators.required]),
+    //     roles: new FormControl('',[Validators.required]),
+    //   });
     }
 
 
@@ -93,4 +175,4 @@ export class NuevoUsuarioComponent implements OnInit {
 
     
   
-}
+
--- src/app/modelo/nuevo-usuario.ts
+++ src/app/modelo/nuevo-usuario.ts
@@ -6,23 +6,15 @@ export class NuevoUsuario {
     nombreUsuario: string;
     email: string;
     password: string;
-    authorities: string[]=[]; 
     roles: string[]=[];
-    activo:boolean = false;
-    fechaCreacion:Date = new Date();
-    fechaActualizacion:Date = new Date();
-    tokenPassword: string = '';
-
-    
-    
-    
-    constructor(nombre: string, apellido: string , dni: string,nombreUsuario: string, email:string, password: string){
+    constructor(nombre: string, apellido: string , dni: string,nombreUsuario: string, email:string, password: string, roles: string[]){
         this.nombre=nombre;
         this.apellido=apellido;
         this.dni=dni;
         this.nombreUsuario=nombreUsuario;
         this.email=email;
         this.password=password;
+        this.roles=roles;
         
 
     }
