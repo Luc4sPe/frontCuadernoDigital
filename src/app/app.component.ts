cba1503712832140d61ec2a7918ae883d44df011
Lucas <luccas.a.pe@gmail.com>
2022-06-14 00:23:02 -0300

    Se realizo la configuración del idioma de ng prime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from './service/token.service';
 
+import { PrimeNGConfig } from 'primeng/api';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,9 +14,51 @@ export class AppComponent implements OnInit{
   title = 'frontCuadernoDigital';
   isLogged = false;
 
-  constructor( private tokenService: TokenService) { }
+
+
+  constructor( private tokenService: TokenService, private primeNgConfig : PrimeNGConfig) { }
 
   ngOnInit(): void {
+
+
+    this.primeNgConfig.ripple = true;
+    this.primeNgConfig.setTranslation({
+      matchAll: 'Coincidir todos',
+      matchAny: 'Coincidir con cualquiera',
+      accept : 'Aceptar',
+      before: 'Antes',
+      after:'Despues',
+      contains:'Contiene',
+      startsWith: 'Comienza con',
+      endsWith: 'Finaliza con',
+      equals: 'Es igual a',
+      notEquals: 'No es igual a',
+      notContains: 'No contiene',
+      lt: 'Menor que',
+      lte: 'Menor o igual que',
+      gt: 'Mayor que',
+      gte: 'Mayor o igual que',
+      dateAfter: 'La fecha es posterior a',
+      dateBefore: 'La fecha es anterior a',
+      dateIs: 'La fecha es',
+      dateIsNot: 'La fecha no es',
+      addRule: 'Agregar regla',
+      removeRule: "Eliminar regla",
+      clear: 'Limpiar',
+      apply: 'Aplicar',
+      dayNames: ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"],
+      dayNamesShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+      dayNamesMin: ["Do","Lu","Ma","Mi","Ju","Vi","Sa"],
+      monthNames: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+      monthNamesShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun","Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+      today: 'Hoy',
+      dateFormat: 'dd/mm/yy'
+    })  
+
+
+
+
+    /*
     if (this.tokenService.getToken()) {
       console.log(this.tokenService.getToken());
       this.isLogged = true;
@@ -22,5 +68,10 @@ export class AppComponent implements OnInit{
   }
   validateLogin($event: boolean): void {
     this.isLogged = $event;
+
+    */
+
+    
   }
+  
 }
