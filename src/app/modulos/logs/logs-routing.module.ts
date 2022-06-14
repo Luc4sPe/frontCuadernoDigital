fc9a75eabe89c8d319761c05b964d7f2b80f9a20
Lucas <luccas.a.pe@gmail.com>
2022-06-13 22:58:03 -0300

    Se genero logMudule y el routingModule

--- src/app/modulos/logs/logs-routing.module.ts
+++ src/app/modulos/logs/logs-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaComponent } from './listado/lista.component';
+import { CulGuardService as guardUsuario } from 'src/app/Core/guards/cul-guard.service';
+import { Roles } from 'src/app/Core/enmus/roles';
 
-const routes: Routes = [];
+const ADMIN = Roles.ADMIN;
+const routes: Routes = [
+  {
+    {
+      path: '',
+      children: [
+        {path: '', redirectTo:'lista'},
+        {
+          path: 'lista',
+          component: ListaComponent,
+          canActivate: [guardUsuario],
+          data: { rolesEsperados: [ADMIN] },
+        },
+        { path: '**', redirectTo: 'lista' },
+      ],
+    },
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modulos/logs/logs.module.ts
+++ src/app/modulos/logs/logs.module.ts
@@ -4,6 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LogsRoutingModule } from './logs-routing.module';
 import { ListaComponent } from './listado/lista.component';
 
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { RouterModule } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import {ScrollTopModule} from 'primeng/scrolltop';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -11,7 +18,14 @@ import { ListaComponent } from './listado/lista.component';
   ],
   imports: [
     CommonModule,
-    LogsRoutingModule
+    LogsRoutingModule,
+    BreadcrumbModule,
+    ButtonModule,
+    TableModule,
+    RouterModule,
+    DropdownModule,
+    ScrollTopModule,
+    CardModule
   ]
 })
 export class LogsModule { }
