a9687e5a1aea1c9a9c57af83991a4a3393d23be9
Lucas <luccas.a.pe@gmail.com>
2023-03-28 18:15:27 -0300

    Se creo la clase y el dto de asesoriaRiego

--- src/app/Core/modelo/asesoria-riego.ts
+++ src/app/Core/modelo/asesoria-riego.ts
@@ -1,2 +1,26 @@
+import { Cuadro } from "./cuadro";
+import { Finca } from "./finca";
+import { Usuario } from "./usuario";
+
 export class AsesoriaRiego {
+
+    id?: number;
+    duracionEnHoras: Date;
+    milimetrosAplicados:number;
+    idCuadros: Cuadro;
+    finca: Finca;
+    productor: Usuario
+    fechaRiego: Date = new Date();
+    fechaModificacionRiego: Date = new Date();
+    
+    constructor( duracionEnHoras: Date,milimetrosAplicados:number, idCuadros: Cuadro,finca: Finca,  productor: Usuario){
+
+        this.duracionEnHoras=duracionEnHoras;
+        this.milimetrosAplicados=milimetrosAplicados;
+        this.idCuadros=idCuadros;
+        this.finca=finca;
+        this.productor=productor;
+          
+    }
+   
 }
