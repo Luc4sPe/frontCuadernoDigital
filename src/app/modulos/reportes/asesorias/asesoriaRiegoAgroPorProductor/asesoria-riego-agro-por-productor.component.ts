b5c6b38dec32a2ed0af5735eb9e8bc9506cf6ef5
Lucas <luccas.a.pe@gmail.com>
2023-10-03 18:54:32 -0300

    Se creo el componente asesoriasRiegoAgroPorProductor

---
+++ src/app/modulos/reportes/asesorias/asesoriaRiegoAgroPorProductor/asesoria-riego-agro-por-productor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asesoria-riego-agro-por-productor',
+  templateUrl: './asesoria-riego-agro-por-productor.component.html',
+  styleUrls: ['./asesoria-riego-agro-por-productor.component.css']
+})
+export class AsesoriaRiegoAgroPorProductorComponent {
+
+}
